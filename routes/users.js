df3a7cf47baac38b97a10994a3b738a97467b0b1
Evlin C <ad81328@gmail.com>
2020-01-31 14:23:04 +0800

    finish users view, making ctl and api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin');
 });
 
 module.exports = router;

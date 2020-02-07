8fac6bdc3b616fc14366db4dd44f6b99803c0272
Evlin C <ad81328@gmail.com>
2020-02-07 11:12:39 +0800

    finish triple match API logic, building DAO logic

--- dao/wantDAO.js
+++ dao/wantDAO.js
@@ -1,13 +1,13 @@
 const mysql = require('../util/mysql');
 
 module.exports = {
-  insert: (wantData)=>{
-  return new Promise((resolve,reject)=>{
-      let insertWantsArr=[];
-      console.log(wantData.matchedArr); 
+  insert: (wantData) => {
+    return new Promise((resolve, reject) => {
+      let insertWantsArr = [];
+      console.log(wantData.matchedArr);
       // make insert rows base on wantItemID
       wantData.wantArr = wantData.wantArr.split(',');
-      wantData.wantArr.forEach( wantItemID => {
+      wantData.wantArr.forEach(wantItemID => {
         // make want row
         let wantItemIDInt = parseInt(wantItemID)
         let requiredInt = parseInt(wantData.required)
@@ -23,7 +23,7 @@ module.exports = {
           wantRow.push('false');
         }
       });
-      mysql.pool.query('INSERT INTO want(want_item_id, want_owner, required_item_id, required_owner, matched) VALUES ?', [insertWantsArr], (err, insertWantResult, fields)=>{
+      mysql.pool.query('INSERT INTO want(want_item_id, want_owner, required_item_id, required_owner, matched) VALUES ?', [insertWantsArr], (err, insertWantResult, fields) => {
         if (err) {
           console.log('error in insertWantPromise');
           console.log(err.sqlMessage);
@@ -39,35 +39,44 @@ module.exports = {
       })
     });
   },
-  get: (queryCondition)=>{
-    // 
+  get: (queryCondition) => {
+    // settings
+    let parseIntArr = [];
+    let queryString = '';
+    let getQueryCondition;
+    queryCondition.item_id = parseInt(queryCondition.item_id);
+    // 判斷查詢類型
     if (queryCondition.wantArr) {
-      // 有 user 申請交換請求時，確認對方有無對自己的商品提出過交換請求
-      return new Promise((resolve,reject)=>{ 
-        queryCondition.item_id = parseInt(queryCondition.item_id);
-        let parseIntArr = [];
-        queryCondition.wantArr.forEach((item_id)=>{
-          parseIntArr.push(parseInt(item_id));
-        })
-        mysql.pool.query(`SELECT * FROM want WHERE want_item_id = ? AND required_item_id in (?)`, [queryCondition.item_id, parseIntArr], (err, checkPreviousWantResult, fileds)=>{
-          if (err) {
-            console.log('error in checkPreviousWantPromise');
-            console.log(err.sqlMessage);
-            console.log(err.sql);
-            reject(err);
-          } else {
-            resolve(checkPreviousWantResult);
-          }
-        })
+      // 查詢指定物品有無針對特定物品群表示過 want
+      queryCondition.wantArr.forEach((item_id) => {
+        parseIntArr.push(parseInt(item_id));
       })
+      queryString = `SELECT * FROM want WHERE want_item_id = ? AND required_item_id in (?)`;
+      getQueryCondition = [queryCondition.item_id, parseIntArr];
     } else {
-      // only search item's wish list
+      console.log('in general want search');
+      // 單純查詢某物品的願望清單
+      queryString = `SELECT * FROM want WHERE want_item_id = ?`
+      getQueryCondition = [queryCondition.item_id];
     }
+    // 有 user 申請交換請求時，確認對方有無對自己的商品提出過交換請求
+    return new Promise((resolve, reject) => {
+      mysql.pool.query(queryString, getQueryCondition, (err, checkPreviousWantResult, fileds) => {
+        if (err) {
+          console.log('error in checkPreviousWantPromise');
+          console.log(err.sqlMessage);
+          console.log(err.sql);
+          reject(err);
+        } else {
+          resolve(checkPreviousWantResult);
+        }
+      })
+    }) 
   },
-  update: (queryCondition)=>{
+  update: (queryCondition) => {
     // 更新 matched column
-    return new Promise((resolve,reject)=>{ 
-      mysql.pool.query(`UPDATE want SET matched = 'true' WHERE want_item_id = ? AND required_item_id in (?)`, [queryCondition.item_id, queryCondition.matchedArr], (err, updateMatchResult, fileds)=>{
+    return new Promise((resolve, reject) => {
+      mysql.pool.query(`UPDATE want SET matched = 'true' WHERE want_item_id = ? AND required_item_id in (?)`, [queryCondition.item_id, queryCondition.matchedArr], (err, updateMatchResult, fileds) => {
         if (err) {
           reject(err);
         } else {
--- routes/wantAPI.js
+++ routes/wantAPI.js
@@ -5,7 +5,10 @@ const wantDAO = require('../dao/wantDAO');
 router.post('/new', async (req, res, next) => {
   // 確認對方是否曾經想要你的物品
   let matched = false;
-  const matchedArr =[];
+  let tripleMatched = false;
+  let matchedArr =[];
+  let tripleMatchedArr =[];
+  let tripleCounter = 0;
   req.body.required_item = parseInt(req.body.required_item);
   const checkMatchResultArr = await wantDAO.get({
     wantArr: req.body.want_items_Arr.split(','),
@@ -29,9 +32,36 @@ router.post('/new', async (req, res, next) => {
     });
   } else {
     // 如果對方沒有想要的，進入三方配對
+    // 取得 middle item wish list (end_items)
     let middleItemWishListArr = await wantDAO.get({
       item_id: req.body.required_item,
     })
+    // foreach 比對 end_items 的個別 wish list 中有無 first_items 存在
+    middleItemWishListArr.forEach((wantBetweenMiddleAndEnd)=>{
+      const endMatchStartResultArr = wantDAO.get({
+        wantArr: wantArr,
+        want_item_id: wantBetweenMiddleAndEnd.required_item,
+      })
+      if (endMatchStartResultArr.length !== 0) {
+        // this end_item match something of start_items
+        console.log('triple match true');
+        tripleMatched = true;
+        endMatchStartResultArr.forEach((wantBetweenEndAndStart)=>{
+          // insert data into 
+          tripleMatchedArr.push({
+            start_item_id: wantBetweenEndAndStart.required_item_id,
+            start_owner: wantBetweenEndAndStart.required_owner,
+            middle_item_id: req.body.required_item,
+            middle_owner: req.body.want_items_owner,
+            end_item_id: wantBetweenEndAndStart.want_item_id,
+            end_owner: wantBetweenEndAndStart.want_owner,
+          });
+          tripleCounter += 1;
+        })
+      } 
+    })
+    // 有 === 三方配對成功，alert訊息並存入 matched table
+    // 無 === 三方配對也失敗， alert 新增訊息
   }
   // Call wantDAO.insert
   const newWantInsertResult = await wantDAO.insert({
@@ -50,6 +80,8 @@ router.post('/new', async (req, res, next) => {
       res.status(200).send({
         msg: `Congratulation, ${checkMatchResultArr.length} items have been added to match result page`,
       })
+    } else if (tripleMatched) {
+      // alert user for triple match
     } else {
       // if not match, only return success insertion msg
       res.status(200).send({

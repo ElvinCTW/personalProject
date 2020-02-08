ebd26b49ce0a166c1234118c36e7436231954cd0
Evlin C <ad81328@gmail.com>
2020-02-08 20:40:09 +0800

    change add_item page res from send to render

--- routes/itemAPI.js
+++ routes/itemAPI.js
@@ -77,8 +77,12 @@ router.post('/new', async (req, res, next) => {
     if (insertItemResult.errorMsg) {
       res.status(500).send(insertItemResult.errorMsg)
     } else {
-      res.status(200).send({
-        insertStatus: 'success',
+      // res.status(200).send({
+      //   insertStatus: 'success',
+      // })
+      // let insertNewItemSuccessMsg = 
+      res.status(200).render('items_add', {
+        successMsg : 'Insert New Item success!'
       })
     }
   });

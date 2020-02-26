22020643c66885192ded5753410e0932a68cf72a
Evlin C <ad81328@gmail.com>
2020-02-26 10:42:37 +0800

    finish get board list function

--- routes/categoryAPI.js
+++ routes/categoryAPI.js
@@ -17,21 +17,21 @@ router.get('/boardList', async (req, res, next) => {
   }
 })
 
-// router.get('/:type', async (req, res, next) => {
-//   // call DAO
-//   let obj = {};
-//   if (req.params.type === 'main') {
-//     obj.action = 'getMainCategory'
-//   } else if (req.params.type === 'sub') {
-//     obj.action = 'getSubCategory'
-//     obj.main_category = req.query.main_category
-//   }
-//   let categoryList = await categoryDAO.get(obj).catch(()=> {
-//     res.send(500).send({errorMsg:'get categoryList error'})
-//   })
-//   if (categoryList) {
-//     res.status(200).send(categoryList)
-//   }
-// })
+router.get('/item_insertion/:type', async (req, res, next) => {
+  // call DAO
+  let obj = {};
+  if (req.params.type === 'main') {
+    obj.action = 'getMainCategory'
+  } else if (req.params.type === 'sub') {
+    obj.action = 'getSubCategory'
+    obj.main_category = req.query.main_category
+  }
+  let categoryList = await categoryDAO.get(obj).catch(()=> {
+    res.send(500).send({errorMsg:'get categoryList error'})
+  })
+  if (categoryList) {
+    res.status(200).send(categoryList)
+  }
+})
 
 module.exports = router;
